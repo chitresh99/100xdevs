2a7d5ccf74fb4bff0571f80df73d2d1dee7df370
chitresh99 <chitreshpoojary990@gmail.com>
2024-06-23 20:29:05 +0530

    learnt and implemented Query parameters

---
+++ Week 2/Express with examples/index.js
@@ -0,0 +1,30 @@
+//creating an http server
+//express
+//node default library => no
+
+const express = require("express");
+
+const app = express();
+
+//functionality :-
+
+function hello(n){
+let ans = 0;
+for(let i = 1; i <=n ;i++){
+ans = ans + i
+}
+return ans;
+}
+
+app.get('/',function(req,res){
+    const n = req.query.n; //query parameters
+    const ans = hello(n);
+    res.send("hi there " +  ans);
+})
+// "/damn" :- is the route
+
+app.get('/damn',function(req,res){
+    res.send("hola at your boy")
+})
+
+app.listen(3000);
