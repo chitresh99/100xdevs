232c1ca5483de4a414d7f050673abf72cdb866da
chitresh99 <chitreshpoojary990@gmail.com>
2024-07-13 22:56:20 +0530

    implemented the middleware

--- Week 4/Mongodb deep dive/03-mongo/middleware/admin.js
+++ Week 4/Mongodb deep dive/03-mongo/middleware/admin.js
@@ -1,7 +1,33 @@
+//get the admin model to check if the admin exists in the admin database
+const {admin, Admin} = require("../db/index")
+
+
 // Middleware for handling auth
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    //the user who was calling this admin middleware
+    //if that username and password exits in the database 
+    // go to the next middleware which is create courses
+
+
+    const username = req.headers.username;
+    const password = req.headers.password
+    
+    //this logic checks if the user exitss
+     Admin.findOne({
+         username : username,
+         password : password
+     })
+    .then(function(value){ //if they do exist
+        if(value){
+            next(); //passing the control to the next middleware
+        }else{
+            res.status(403).json({
+                msg : "user doesn't exist"
+            })
+        }
+    })
 }
 
 module.exports = adminMiddleware;
--- Week 4/Mongodb deep dive/03-mongo/middleware/user.js
+++ Week 4/Mongodb deep dive/03-mongo/middleware/user.js
@@ -1,6 +1,32 @@
+const {User} = require("../db/index")
+
+//admin middleware checks if the admin is there with that username and password
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    //the user who was calling this admin middleware
+    //if that username and password exits in the database 
+    // go to the next middleware which is create courses
+
+
+    const username = req.headers.username;
+    const password = req.headers.password
+    
+    //this logic checks if the user exitss
+     User.findOne({
+         username : username,
+         password : password
+     })
+    .then(function(value){ //if they do exist
+        if(value){
+            next(); //passing the control to the next middleware
+        }else{
+            res.status(403).json({
+                msg : "user doesn't exist"
+            })
+        }
+    })
 }
 
 module.exports = userMiddleware;
