1d7aa0ab6be53f24173a2d36f6c877a7a5b8e3d0
chitresh99 <chitreshpoojary990@gmail.com>
2024-07-15 19:04:16 +0530

    fixing router

---
+++ Week 4/04-mongo-with-jwt-auth/middleware/admin.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../index"); //you can either write ../index or just ..
+
+// Middleware for handling auth
+function adminMiddleware(req, res, next) {
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+    const token = req.headers.authorization;
+    const words = token.split(" "); // making an array of [bearer, asadsasda (token)]
+    const jwtToken = words[1]; //get the token from the array 
+    const decodedvalue = jwt.verify(jwtToken,secret);
+    if(decodedvalue.username){
+        next();
+    }else{
+        res.status(403).json({
+            message : "You are not authenticated"
+        })
+    }
+
+    //get the token
+}
+
+module.exports = adminMiddleware;
---
+++ Week 4/04-mongo-with-jwt-auth/middleware/user.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const secret = require("../index");
+
+function userMiddleware(req, res, next) {
+    // Implement user auth logic
+    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+
+    const token = req.headers.authorization;
+    const words = token.split(" "); // making an array of [bearer, asadsasda (token)]
+    const jwtToken = words[1]; //get the token from the array 
+    const decodedvalue = jwt.verify(jwtToken,secret);
+
+    //username,type : "admin" | "user" //ideally
+   
+    // There is a difference between authentication and authorization
+
+    if(decodedvalue.username){
+        next();
+    }else{
+        res.status(403).json({
+            message : "You are not authenticated"
+        })
+    }
+}
+
+module.exports = userMiddleware;
