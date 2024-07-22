fa4a9f255ecc27479af507ab7d4060eaeac142b3
chitresh99 <chitreshpoojary990@gmail.com>
2024-07-22 22:53:51 +0530

    created a counter app

--- Week 5/React Project/counter-app/src/App.jsx
+++ Week 5/React Project/counter-app/src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css'
+//this is called a hook
+import { useState } from 'react';
 
+//defining the global state
+// let state =  {
+//     count : 0
+// }
+
+
+//button which renders state.count
 function App() {
+    
+    //initialising the state
+    const [count, setCount] = useState(0);
+
+    function onclickhander(){
+        setCount(count + 1); //setting the state
+    }
+    
+    //returning the dynamic html
  return (
   <div>
-  hi there
+  <button onClick={onclickhander}>Counter {count}</button>
   </div>
  )
 }
