6d67de5c5c432d7f3d35313abea2040094e01dbf
chitresh99 <chitreshpoojary990@gmail.com>
2024-07-26 20:16:14 +0530

    jsx syntax

---
+++ Self learning/React learning/jsxsyntax/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  const style = {
+    color: 'blue',
+    backgroundColor: 'lightgray'
+  };
+  
+  return (
+   <>
+   <h1 style={style}>Styled Heading</h1>
+   </>
+  )
+}
+
+export default App
