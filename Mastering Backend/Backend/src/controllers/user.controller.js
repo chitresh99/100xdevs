7302b81e1d9483a1c832643db16cdd2602e5c2df
chitresh99 <chitreshpoojary990@gmail.com>
2024-09-07 23:02:28 +0530

    added a controller

--- Mastering Backend/Backend/src/controllers/user.controller.js
+++ Mastering Backend/Backend/src/controllers/user.controller.js
@@ -3,9 +3,16 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 //method
 //higher order function which takes function as a parameter
 const registerUser = asyncHandler( async (req,res) => {
-return res.status(200).json({
-    message:"ok"
-})
+  //get user from frontend
+  //validation - not empty
+  //check if user already existsa:check from username and email
+  //check for images,check for avatar
+  //upload them to cloudinary
+  //create user object - create entry in db
+  //remove password and refresh token field from response
+  //remove password and refresh token field from response
+  //check for user creation
+  //return response
 })
 
 export {registerUser}
---
+++ Week 6/week-6-experiments/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <Header title = "chitresh 1"></Header>
+      <Header title = "chitresh 2"></Header>
+    </div>
+  )
+}
+
+  
+
+export default App
